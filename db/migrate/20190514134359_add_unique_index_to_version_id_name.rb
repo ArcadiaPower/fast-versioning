700ba73bcd3685ee6fd4604f4509ee401c34e98c
Iwo Dziechciarow <iiwo@o2.pl>
2019-05-14 21:35:59 +0200

    create the index concurently

--- db/migrate/20190514134359_add_unique_index_to_version_id_name.rb
+++ db/migrate/20190514134359_add_unique_index_to_version_id_name.rb
@@ -1,5 +1,7 @@
 class AddUniqueIndexToVersionIdName < ActiveRecord::Migration[5.0]
+  disable_ddl_transaction!
+
   def change
-    add_index :fast_versioning_fast_versions, %i[version_id name], unique: true
+    add_index :fast_versioning_fast_versions, %i[version_id name], unique: true, algorithm: :concurrently
   end
 end
