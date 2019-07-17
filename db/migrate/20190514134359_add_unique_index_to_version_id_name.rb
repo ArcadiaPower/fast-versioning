c9df47b96737bebcdb4158bc1609b12f60a44797
Iwo Dziechciarow <iiwo@o2.pl>
2019-07-18 00:23:03 +0200

    basic travis config

--- db/migrate/20190514134359_add_unique_index_to_version_id_name.rb
+++ db/migrate/20190514134359_add_unique_index_to_version_id_name.rb
@@ -1,7 +1,5 @@
 class AddUniqueIndexToVersionIdName < ActiveRecord::Migration[5.0]
-  disable_ddl_transaction!
-
   def change
-    add_index :fast_versioning_fast_versions, %i[version_id name], unique: true, algorithm: :concurrently
+    add_index :fast_versioning_fast_versions, %i[version_id name], unique: true
   end
 end
