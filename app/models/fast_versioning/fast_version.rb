79cf5440087430cad22b804c9ba0cd91db0e5713
justindoody <stickmedia@gmail.com>
2019-09-09 19:12:16 -0400

    explicit optional for whodunnit in order to support belongs_to_required_by_default option in recent rails versions

--- app/models/fast_versioning/fast_version.rb
+++ app/models/fast_versioning/fast_version.rb
@@ -1,7 +1,7 @@
 module FastVersioning
   class FastVersion < ActiveRecord::Base
     belongs_to :item, polymorphic: true
-    belongs_to :whodunnit, polymorphic: true
+    belongs_to :whodunnit, polymorphic: true, optional: true
     belongs_to :version, class_name: 'PaperTrail::Version'
 
     serialize :meta, JSON
